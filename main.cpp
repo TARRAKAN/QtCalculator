c5ca1631f458154135b272b0afc3b54c4118ad90
Vladimir Lebedev <vovalebedev@bk.ru>
2021-07-21 15:20:47 +0300

    pushed from local

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "calculator.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Calculator calculator;
+    calculator.setWindowTitle("Calculator");
+    calculator.resize(230,200);
+    calculator.show();
+    return a.exec();
+}
